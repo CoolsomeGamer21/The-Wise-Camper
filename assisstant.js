b6ae9b4f531e5250054aa305eb28f48aab0458ce
CoolsomeGamer21 <105989804+CoolsomeGamer21@users.noreply.github.com>
2022-07-29 16:24:24 -0400

    Update assisstant.js

--- assisstant.js
+++ assisstant.js
@@ -1,10 +1,14 @@
 JavaScript default 
 
- let idle = stanby
- let buttonPressed = ''
- 
+ let idle = stanby;
+ let buttonPressed = '';
+ const command = '';
  if (buttonPressed === true) {
-   console.log('What Can I do for you?')
+   console.log('What Can I do for you?');
  } else {
    console.log(idle)
 
+if(buttonPressed, command === true) {
+ console.log('Navigating to page');
+} else {
+ console.log('I don't understand, try again?');
